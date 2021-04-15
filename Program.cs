dcdfff2041e65e3d380d0795b3e408769f58d503
Shain-Allen <48696021+Shain-Allen@users.noreply.github.com>
2021-04-14 22:08:01 -0700

    small progress

--- Program.cs
+++ Program.cs
@@ -6,12 +6,34 @@ namespace InputValidation
     {
         static void Main(string[] args)
         {
+            int sum = 0;
+            Console.WriteLine("please input 3 whole numbers that you wish to get the sum of. hit enter after each number");
 
+            for (int i = 0; i < 3; i++)
+            {
+                sum += Int32.Parse(Console.ReadLine());
+            }
         }
 
         char GradeConverter(int numberGrade)
         {
-            
+            switch (numberGrade)
+            {
+                case 100:
+                case 91:
+                    return 'A';
+                case 90:
+                case 81:
+                    return 'B';
+                case 80:
+                case 71:
+                    return 'C';
+                case 70:
+                case 61:
+                    return 'D';
+                default:
+                    return 'F';
+            }
         }
     }
 }
